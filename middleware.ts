97c7dc0adbfc78268339b04c5b9b9fd0dde7fc96
stonefolk663-lang <stonefolk663@gmail.com>
2026-05-12 06:58:54 -0700

    middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,3 +1,4 @@
+export const runtime = 'experimental-edge';
 import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs'
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
